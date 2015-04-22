73958b490fad1fa2db939313856edb7d34067790
Axel Hernández Ferrera <axelhzf@gmail.com>
2015-04-22 22:17:54 +0100

    demo code

---
+++ demo/Demo/index.ios.js
@@ -0,0 +1,64 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} = React;
+
+var Demo = React.createClass({
+  render: function() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+Control+Z for dev menu
+        </Text>
+      </View>
+    );
+
+
+/*    <Image
+      style={styles.logo}
+      source={{uri: 'http://facebook.github.io/react/img/logo_og.png'}}
+      />*/
+
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5
+  },
+  logo: {
+    width: 100,
+    height: 100
+  }
+});
+
+AppRegistry.registerComponent('Demo', () => Demo);
